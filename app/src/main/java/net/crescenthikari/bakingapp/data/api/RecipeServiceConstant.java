d7297cce6e1bc9df76b1c4447be9b4b2cfb070c6
Muhammad Fiqri <viqronization@gmail.com>
2017-10-11 11:22:34 +0700

    make class final & class constructor private

--- app/src/main/java/net/crescenthikari/bakingapp/data/api/RecipeServiceConstant.java
+++ app/src/main/java/net/crescenthikari/bakingapp/data/api/RecipeServiceConstant.java
@@ -4,6 +4,9 @@ package net.crescenthikari.bakingapp.data.api;
  * Created by Muhammad Fiqri Muthohar on 9/16/17.
  */
 
-public class RecipeServiceConstant {
+public final class RecipeServiceConstant {
     public static final String BASE_URL = "https://d17h27t6h515a5.cloudfront.net";
+
+    private RecipeServiceConstant() {
+    }
 }
